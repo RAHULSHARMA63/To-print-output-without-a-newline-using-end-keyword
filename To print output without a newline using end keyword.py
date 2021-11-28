e5ee51b366782fb215b2b4ceb8ba6a2f9a9ac406
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2021-11-28 19:54:46 +0530

    To print output without a newline using end keyword.

---
+++ To print output without a newline using end keyword.py
@@ -0,0 +1,10 @@
+# print each statement on a new line
+print("English")
+print("is easy to learn.")
+
+# new line
+print()
+
+# print both the statements on a single line
+print("English", end=" ")
+print("is easy to learn.")
